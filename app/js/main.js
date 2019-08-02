26abd45e7ce79ffe7cf6f8d21297d09d5304ca50
Fzovpec <obgpsuper@gmail.com>
2019-08-02 17:25:50 +0300

    Sliders

---
+++ app/js/main.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper('#swiper-container_1', {
+  slidesPerView: 2,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '#swiper-button-next_1',
+      prevEl: '#swiper-button-prev_1',
+    },
+});
+var swiper_1 = new Swiper('#swiper-container_2', {
+  slidesPerView: 2,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '#swiper-button-next_2',
+      prevEl: '#swiper-button-prev_2',
+    },
+});
+var swiper_2 = new Swiper('#swiper-container_3', {
+  slidesPerView: 2,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '#swiper-button-next_3',
+      prevEl: '#swiper-button-prev_3',
+    },
+});
+var swiper_3 = new Swiper('#swiper-container_4', {
+  slidesPerView: 2,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '#swiper-button-next_4',
+      prevEl: '#swiper-button-prev_4',
+    },
+});
