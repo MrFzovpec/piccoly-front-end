f365619839d044144c25953ec510a304fbc69745
Глеб <glebfen@gmail.com>
2019-08-02 17:27:43 +0200

    Swiper

--- app/js/main.js
+++ app/js/main.js
@@ -30,3 +30,19 @@ var swiper_3 = new Swiper('#swiper-container_4', {
       prevEl: '#swiper-button-prev_4',
     },
 });
+var swiper_4 = new Swiper('#big_swiper', {
+  slidesPerView: 1,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '#swiper-button-next_1',
+      prevEl: '#swiper-button-prev_1',
+    },
+});
+var swiper_5 = new Swiper('#slider_3', {
+  slidesPerView: 3,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '#slider_3_next',
+      prevEl: '#slider_3_prev',
+    },
+});
---
+++ app/js/swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 2,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+var swiper_1 = new Swiper('.swiper-container_1', {
+  slidesPerView: 2,
+  direction: 'horizontal',
+  navigation: {
+      nextEl: '.swiper-button-next_1',
+      prevEl: '.swiper-button-prev_1',
+});
+    
